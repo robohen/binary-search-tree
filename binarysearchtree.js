70fc53fb99e27cf76050095ce5ae9e0b9d1fb67e
robohen <88636103+robohen@users.noreply.github.com>
2022-09-18 22:25:32 -0700

    just the code for a binary search tree and different orders explained within the document

---
+++ binarysearchtree.js
@@ -0,0 +1,124 @@
+// Implementation of binary Search Tree
+// use of recursion ahead
+
+// class representing unit node of Binary Search Tree
+class Node {
+	constructor ( data ) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+// class representing Binary Search Tree
+class BST {
+	constructor () { this.root = null; }
+
+	// function to initialize BST with node
+	insert ( data ) {
+		var newNode = new Node( data );
+		if ( this.root === null )
+			this.root = newNode;
+		else
+			this.insertNode( this.root, newNode );
+	}
+
+	// function to perform insertion on BST
+	insertNode ( node, newNode ) {
+		if ( newNode.data < node.data ) {
+			if ( node.left === null )
+				node.left = newNode;
+			else
+				this.insertNode( node.left, newNode );
+		}
+		else {
+			if ( node.right === null )
+				node.right = newNode;
+			else
+				this.insertNode( node.right, newNode );
+		}
+	}
+
+	// method to call removeNode()
+	remove ( data ) { this.root = this.removeNode( this.root, data ) }
+
+	// function to remove given node from BST
+	removeNode ( node, key ) {
+		if ( node === null )
+			return null;
+		else if ( key < node.data ) {
+			node.left = this.removeNode( node.left, key );
+			return node;
+		}
+		else if ( key > node.data ) {
+			node.right = this.removeNode( node.right, key );
+			return node;
+		}
+		else {
+			//if no child node
+			if ( node.left === null && node.right === null ) {
+				node = null;
+				return node;
+			}
+			// one child node
+			if ( node.left === null ) {
+				node = node.right;
+				return node;
+			}
+			else if ( node.right === null ) {
+				node = node.right;
+				return node;
+			}
+			// two child nodes
+			let aux = this.findMinNode( node.right );
+			node.data = aux.data;
+			node.right = this.removeNode( node.right, aux.data );
+			return node;
+		}
+	}
+
+	// method to execute inorder traversal
+	inorder ( node ) {
+		this.inorder( node.left );
+		console.log( node.data );
+		this.inorder( node.right );
+	}
+
+	// method to execute preorder traversal
+	preorder ( node ) {
+		console.log( node.data );
+		this.preorder( node.left );
+		this.preorder( node.right );
+	}
+
+	// method to execute postorder traversal
+	postorder ( node ) {
+		if ( node === null ) return;
+		this.postorder( node.left );
+		this.postorder( node.right );
+		console.log( node.data );
+	}
+
+	// to find minimum node in BST
+	findMinNode ( node ) {
+		if ( node.left === null )
+			return node;
+		else
+			return this.findMinNode( node.left );
+	}
+
+	// to get root of BST 
+	getRootNode () { return this.root; }
+
+	// to search for a node with given data
+	search ( node, data ) {
+		if ( node === null )
+			return null;
+		else if ( data < node.data )
+			return this.search( node.left, data );
+		else if ( data > node.data )
+			return this.search( node.right, data );
+		else
+			return node;
+	}
+}
